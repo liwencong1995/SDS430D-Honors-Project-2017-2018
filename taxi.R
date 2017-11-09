15318181f1ee69c966e7104bd5315cfc27b9b996
liwencong1995 <liwencong1995@gmail.com>
2017-11-09 00:24:08 -0500

    Load three types of transporatation's data into MySQL.

--- taxi.R
+++ taxi.R
@@ -6,19 +6,26 @@ library(nyctaxi)
 library(RMySQL)
 #mysql connection
 db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P320718")
-taxi <- etl("nyctaxi", dir = "~/Desktop/nyctaxi/", db)
+taxi <- etl("nyctaxi", dir = "/Volumes/UNTITLED/Honors/nyctaxi", db)
 
 #Download the data I need for my study
+#yellow taxi 2015
 taxi %>%
-  etl_extract(years = 2015, months = 1:12, types = c("green","yellow")) %>% 
-  etl_transform(years = 2015, months = 1:12, types = c("green","yellow")) %>% 
-  etl_load(years = 2015, months = 1:12, types = c("green","yellow"))
+  etl_extract(years = 2015, months = 1:12, types = c("yellow")) %>% 
+  etl_transform(years = 2015, months = 1:12, types = c("yellow")) %>% 
+  etl_load(years = 2015, months = 1:12, types = c("yellow"))
 
 taxi %>%
-  etl_extract(years = 2014, months = 1:12, types = c("yellow", "green")) %>% 
-  etl_transform(years = 2014, months = 1:12, types = c("yellow", "green")) %>% 
-  etl_load(years = 2014, months = 1:12, types = c("yellow","green"))
+  tbl("yellow") %>%
+# green taxi 2015
+taxi %>%
+  etl_extract(years = 2015, months = 1:12, types = c("green")) %>% 
+  etl_transform(years = 2015, months = 1:12, types = c("green")) %>% 
+  etl_load(years = 2015, months = 1:12, types = c("green"))
 
+taxi %>%
+  tbl("green") %>%
+  
 taxi %>%
   etl_extract(years = 2011, months = 1:12, types = c("yellow")) %>% 
   etl_transform(years = 2011, months = 1:12, types = c("yellow")) %>% 
--- uber.R
+++ uber.R
@@ -1,6 +1,16 @@
-install.packages("devtools")
-devtools::install_github("beanumber/nyctaxi")
-library(nyctaxi)
-taxi <- etl("nyctaxi", dir = "~/Desktop/nyctaxi/")
+#Creat the connection
+library(RMySQL)
+#mysql connection
+db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P320718")
+taxi <- etl("nyctaxi", dir = "/Volumes/UNTITLED/Honors/nyctaxi", db)
+
 taxi %>%
-  etl_extract(years = 2014:2015, months = 1:12, transportation = "uber") 
+  etl_extract(years = 2015, months = 1:12, types = c("yellow"), transportation = "uber") %>% 
+  etl_transform(years = 2015, months = 1:12, types = c("yellow"), transportation = "uber") %>% 
+  etl_load(years = 2015, months = 1:12, types = c("yellow"), transportation = "uber")
+
+taxi %>% 
+  etl_extract(years = 2014, months = 1:12, types = c("green"), transportation = "uber") %>% 
+  etl_transform(years = 2014, months = 1:12, types = c("green"), transportation = "uber") %>% 
+  etl_load(years = 2014, months = 1:12, types = c("green"), transportation = "uber") 
+
