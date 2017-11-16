691d90528510490d2a7c5347efd2daf8ca2cb2c9
liwencong1995 <liwencong1995@gmail.com>
2017-11-16 12:44:39 -0500

    tested transform function for green taxi. get the data ready for load #23

--- lyft.R
+++ lyft.R
@@ -5,8 +5,10 @@ db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P
 taxi <- etl("nyctaxi", dir = "/Volumes/UNTITLED/Honors/nyctaxi", db)
 
 taxi %>%
-  etl_extract(years = 2015:2017, months = 1:12, types = c("yellow"), transportation = "lyft") %>% 
-  etl_transform(years = 2015:2017, months = 1:12, types = c("yellow"), transportation = "lyft") %>% 
+  etl_extract(years = 2015:2017, months = 1:12, transportation = "lyft") %>% 
+  etl_transform(years = 2015:2017, months = 1:12, transportation = "lyft")
+
+taxi %>%  
   etl_load(years = 2015:2017, months = 1:12, types = c("yellow"), transportation = "lyft")
 
 lyft <- taxi %>% tbl("lyft") %>% collect()
--- taxi.R
+++ taxi.R
@@ -8,29 +8,34 @@ library(RMySQL)
 db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P320718")
 taxi <- etl("nyctaxi", dir = "/Volumes/UNTITLED/Honors/nyctaxi", db)
 
-taxi <- etl("nyctaxi", dir = "~/Desktop/nyctaxi", db)
-
-
+#taxi <- etl("nyctaxi", dir = "~/Desktop/nyctaxi", db)
 #Download the data I need for my study
-#yellow taxi 2015
+#green taxi 2015
 taxi %>%
-  etl_extract(years = 2015, months = 1, types = c("yellow")) %>% 
-  etl_transform(years = 2015, months = 1, types = c("yellow")) %>%
-  etl_load(years = 2015, months = 1, types = c("yellow"))
+  etl_extract(years = 2015, months = 1:12, types = c("green")) %>% 
+  etl_transform(years = 2015, months = 1:12, types = c("green"))
 
-taxi%>%
-  etl_init() %>% 
-  etl_load(years = 2015, months = 1:12, types = c("yellow"))
+taxi %>%
+  etl_init()
+
+taxi %>%
+  etl_load(years = 2015, months = 1:12, types = c("green"))
   
-
 taxi %>%
   tbl("yellow")
 
-# green taxi 2016 testing etl_load
+# green taxi: testing etl_load
 taxi %>%
-  etl_extract(years = 2016, months = 1, types = c("green")) %>% 
-  etl_transform(years = 2016, months = 1, types = c("green")) %>% 
-  etl_load(years = 2016, months = 1, types = c("green"))
+  etl_extract(years = 2014:2016, months = 4, types = c("green")) %>% 
+
+taxi%>%  
+  etl_transform(years = 2015, months = 1:12, types = c("green"))
+
+taxi %>%
+  etl_init()
+
+taxi%>%
+  etl_load(years = 2014:2016, months = 4, types = c("green"))
 
 #testing sqlite
 taxi %>%
