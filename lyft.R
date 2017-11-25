cf4b1d4affee2a5bdd7c9dd58652ebbd4686eaff
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2017-11-25 11:47:01 -0500

    Test `etl_extract` for lyft.

--- lyft.R
+++ lyft.R
@@ -1,5 +1,6 @@
 #Creat the connection
 library(RMySQL)
+library(nyctaxi)
 #mysql connection
 db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P320718")
 
@@ -17,6 +18,9 @@ taxi %>%
 #Prep data for data analysis
 db <- src_mysql("nyctaxi", user = "wli37", host = "scidb.smith.edu", password = "Calculati0n")
 taxi <- etl("nyctaxi", dir = "/Volumes/UNTITLED/Honors/nyctaxi", db)
+
+taxi %>%
+  etl_extract(years = 2015:2016, months = 1:12, type = "lyft")
 
 taxi %>%  
   etl_load(years = 2015:2016, months = 1:12, type = "lyft")
